7f99ad331419d2418ab7c5c276d599643b0ea2c7
gluonMaster <156231487+gluonMaster@users.noreply.github.com>
2024-01-12 21:53:48 +0100

    Update verify.js

--- sources/verify.js
+++ sources/verify.js
@@ -2,15 +2,37 @@ const inputField = document.getElementById('inputField');
 const referenceWord = 'Hallo';
 
 inputField.addEventListener('input', () => {
-    const enteredText = inputField.value;
+    const enteredText = inputField.innerText;
+    let l=enteredText.length;
 
-    for (let i = 0; i < enteredText.length; i++) {
-        if (referenceWord[i] === enteredText[i]) { 
-            inputField.classList.remove('incorrect');
-            inputField.classList.add('correct');
+    for (let i = 0; i < l; i++) {
+
+        if (referenceWord[i] === enteredText[i]) {
+            let range = document.createRange();
+            range.setStart(inputField.childNodes[0], i);
+            range.setEnd(inputField.childNodes[0], i+1);
+            let selection = window.getSelection();
+            selection.addRange(range)
+            //selection.getRangeAt(0);
+            //inputField.setSelectionRange(i, i + 1);
+            document.execCommand('foreColor', false, 'green');
+            selection.removeAllRanges();
+            range=null
+            //inputField.classList.remove('incorrect');
+            //inputField.classList.add('correct');
         } else {
-            inputField.classList.remove('correct');
-            inputField.classList.add('incorrect');
+            let range = document.createRange();
+            range.setStart(inputField.childNodes[0], i);
+            range.setEnd(inputField.childNodes[0], i+1);
+            let selection = window.getSelection();
+            selection.addRange(range);
+            //inputField.setSelectionRange(i, i + 1);
+            document.execCommand('foreColor', false, 'red');
+            selection.removeAllRanges();
+            range=null
+            //inputField.classList.remove('correct');
+            //inputField.classList.add('incorrect');
         }
     }
 });
+
